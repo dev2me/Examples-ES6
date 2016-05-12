53e8c91b2e9bd4fbf65e0e42cd2d6a246f6312e2
David Hackro <hackro91@gmail.com>
2016-05-12 17:26:48 -0500

    1.4 Operadores Logicos

--- es6.js
+++ es6.js
@@ -1,5 +1,24 @@
-//Example 04
-//Operadores Unarios
+//Example 05
+//Operadores Logicos
 
-console.log(typeof 4);
-console.log(-(10-8));
+console.log(5 == 1);
+console.log(5 <= 1);
+console.log(5 < 1);
+console.log(5 > 1);
+console.log(5 >= 1);
+console.log(5 == 1 && 4 > 6);
+console.log(5 < 1 || 5 == 5);
+
+
+
+/*
+== igual
+!= diferente
+<  menor que 
+<= meyor igual
+>  mayor que
+>= mayor igual
+%% and
+|| or
+!  not
+*/
