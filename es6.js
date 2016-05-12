29847e8c8e3c78d4b0f7ad058936f55a900c391e
David Hackro <hackro91@gmail.com>
2016-05-12 17:03:39 -0500

    1.1 values

--- es6.js
+++ es6.js
@@ -1,2 +1,12 @@
-let saludo = 'Hola Mundo';
-console.log(saludo);
+//Example 02
+//Values
+
+let cadena = 'Tutorials Hackro';
+let integer = 2;
+let double = 19.5;
+let boolean = true;
+
+console.log(cadena);
+console.log(integer);
+console.log(double);
+console.log(boolean);
