2f11546ffcdf8572ecd3e14720eaeea1b35f24c3
David Hackro <hackro91@gmail.com>
2016-05-13 11:45:25 -0500

    1.15 default parameters

--- es6.js
+++ es6.js
@@ -1,22 +1,7 @@
-//`Example 14
-//Functions
+//`Example 15
+//defaul parameters 
 
-function sayHello(name)
-{
-		console.log(`say!! ${name}`)
+function people(name ="david",age="19"){
+	console.log(`Hello my name is ${name} and i have ${age} years old`);
 }
-
-
-var sayHi = function(name)
-{
-	console.log(`hey how are you! ${name}`);
-}
-
-
-var resultOperation = function(value1,value2){
-	return value1 * value2;
-}
-
-sayHello(`david`);
-sayHi(`hackro`);
-console.log(`the result is ${resultOperation(2,3)}`);
+people();
